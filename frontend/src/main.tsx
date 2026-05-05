d14c2646b28a263b315d169b0c02accbbbb79c36
ChristianNemeno <christiannemeno80@gmail.com>
2026-05-05 17:10:45 +0800

    feat: initialize git repository and configure project structure for dapp development

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MeshProvider } from "@meshsdk/react";
 import App from "./App";
+import "@meshsdk/react/styles.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MeshProvider>
+      <App />
+    </MeshProvider>
   </React.StrictMode>
 );
