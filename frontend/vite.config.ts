ad43ef7095e3d13d8fa5428b8788fdc4033d40cb
ChristianNemeno <christiannemeno80@gmail.com>
2026-05-05 17:10:36 +0800

    feat: integrate Mesh SDK for blockchain wallet connectivity and project scaffolding

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    nodePolyfills()
+  ],
+  define: {
+    global: 'globalThis',
+  },
 })
